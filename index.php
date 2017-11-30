6670f465450cd81ab7b01da34ac03f7fa26069b6
Doni0 <doniagustian0@gmail.com>
2017-11-30 16:44:26 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 /*
 copyright @ medantechno.com
 Modified by Ilyasa
+And Modified by Farzain - zFz ( Faraaz )
 2017
 */
 require_once('./line_class.php');
 
-$channelAccessToken = 'YOUR-CHANNEL-ACCESS-TOKEN'; //Your Channel Access Token
-$channelSecret = 'YOUR-CHANNEL-SECRET';//Your Channel Secret
+$channelAccessToken = '7v5dPC2BO0AVyHF2/i5PF8jy9Goa0XJ6DGbpHEcoM0pwahBUR8XVl8hAWGc7TIBeHwcPN+2k8CCER70dq68/XwQ8XJ31hLHfXkfFYjnonO2V5sVfNtJJvAmFYGX5SpRGMmqSWfpEkr2AHZmwhtDh0AdB04t89/1O/w1cDnyilFU='; //Your Channel Access Token
+$channelSecret = '76515f1bbbf41cf8317f9e5e6cbcb6fe';//Your Channel Secret
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
@@ -34,7 +35,7 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = 'YOUR-API-KEY-SIMSIMI'; //API SimSimi
+$key = '7716b4e9-7ddb-4814-a144-5282035d68ce'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
